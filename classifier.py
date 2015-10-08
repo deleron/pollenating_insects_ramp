49d8bc652bb5477caaeb532dc43af6aec3b6dcf3
Florian Lesaint <deleron@gmail.com>
2015-10-08 13:46:59 +0200

    Hedi's model with flip and rotation

--- classifier.py
+++ classifier.py
@@ -1,26 +1,115 @@
-import numpy as np
+import os
+os.environ["THEANO_FLAGS"] = "device=gpu"
 from sklearn.base import BaseEstimator
-from sklearn.ensemble import RandomForestClassifier
+import os
+from lasagne import layers, nonlinearities
+from lasagne.updates import nesterov_momentum, rmsprop, adagrad
+from nolearn.lasagne import NeuralNet, BatchIterator
+from nolearn.lasagne.handlers import EarlyStopping
+import numpy as np
+from caffezoo.googlenet import GoogleNet
+from itertools import repeat
+from sklearn.pipeline import make_pipeline
 
-## Simple Random Forest Classifier
+def sample_from_rotation_x(x):
+    x_extends = []
+    for i in range(x.shape[0]):
+        x_extends.extend([
+        np.array([x[i,:,:,0], x[i,:,:,1], x[i,:,:,2]]),
+        np.array([np.rot90(x[i,:,:,0]),np.rot90(x[i,:,:,1]), np.rot90(x[i,:,:,2])]),
+        np.array([np.rot90(x[i,:,:,0],2),np.rot90(x[i,:,:,1],2), np.rot90(x[i,:,:,2],2)]),
+        np.array([np.rot90(x[i,:,:,0],3),np.rot90(x[i,:,:,1],3), np.rot90(x[i,:,:,2],3)])
+        ])
+    x_extends = np.array(x_extends) #.transpose((0, 2, 3, 1))
+    return x_extends
+
+def sample_from_rotation_y(y):
+    y_extends = []
+    for i in y:
+        y_extends.extend( repeat( i ,4) )
+    return np.array(y_extends)
+
+
+class FlipBatchIterator(BatchIterator):
+    def transform(self, Xb, yb):
+        Xb, yb = super(FlipBatchIterator, self).transform(Xb, yb)
+        # Flip half of the images in this batch at random:
+        bs = Xb.shape[0]
+        indices = np.random.choice(bs, bs / 2, replace=False)
+        Xb[indices] = Xb[indices, :, ::-1, :]
+        return Xb, yb
+
+def build_model():    
+    L=[
+        #(layers.InputLayer, {'shape':(None, 3, 64, 64)}),
+        (layers.InputLayer, {'shape':(None, 3, 64, 64)}),
+        (layers.Conv2DLayer, {'num_filters':32, 'filter_size':(2,2), 'pad':0}),
+        (layers.MaxPool2DLayer, {'pool_size': (3, 3)}),
+        (layers.Conv2DLayer, {'num_filters':32, 'filter_size':(2,2), 'pad':0}),
+        (layers.MaxPool2DLayer, {'pool_size': (2, 2)}),
+        (layers.Conv2DLayer, {'num_filters':16, 'filter_size':(2,2), 'pad':0}),
+        (layers.MaxPool2DLayer, {'pool_size': (1, 1)}),
+        (layers.DenseLayer, {'num_units': 512, 'nonlinearity':nonlinearities.leaky_rectify}),
+        (layers.DropoutLayer, {'p':0.5}),
+        (layers.DenseLayer, {'num_units': 512, 'nonlinearity':nonlinearities.leaky_rectify}),
+        (layers.DropoutLayer, {'p':0.5}),
+        (layers.DenseLayer, {'num_units': 256, 'nonlinearity':nonlinearities.tanh}),
+        (layers.DropoutLayer, {'p':0.2}),
+        (layers.DenseLayer, {'num_units': 18, 'nonlinearity':nonlinearities.softmax}),
+    ]
+
+
+    net = NeuralNet(
+        layers=L,
+        update=adagrad,
+        update_learning_rate=0.01,
+        use_label_encoder=True,
+        verbose=1,
+        max_epochs=50,
+        batch_iterator_train=FlipBatchIterator(batch_size=256),
+        on_epoch_finished=[EarlyStopping(patience=50, criterion='valid_loss')]
+        )
+    return net
+
+# currently used
+def keep_dim(layers):
+    #print len(layers), layers[0].shape
+    return layers[0]
+
 class Classifier(BaseEstimator):
+
     def __init__(self):
-        self.parameters = {
-            'n_estimators' : 10,
-            'max_features' : 2,
-            'max_leaf_nodes' : 5
-        }
-        pass
- 
+        self.net = make_pipeline(
+            #GoogleNet(aggregate_function=keep_dim, layer_names=["input"]),
+            build_model()
+        )
+        
+    def data_augmentation(self, X, y):
+        X = sample_from_rotation_x(X)
+        y = sample_from_rotation_y(y)
+        return X, y
+
+    def preprocess(self, X, transpose=True):
+        X = (X / 255.)
+        X = X.astype(np.float32)
+        if transpose:
+            X = X.transpose((0, 3, 1, 2))
+        return X
+    
+    def preprocess_y(self, y):
+        return y.astype(np.int32)
+
     def fit(self, X, y):
-        X_vectorized = X.reshape((X.shape[0], X.shape[1] * X.shape[2] * X.shape[3]))    
-        self.clf = RandomForestClassifier(**self.parameters)
-        self.clf.fit(X_vectorized, y)
- 
+        X, y = self.preprocess(X, False), self.preprocess_y(y)
+        X, y = self.data_augmentation(X, y)
+        self.net.fit(X, y)
+        return self
+
     def predict(self, X):
-        X_vectorized = X.reshape((X.shape[0], X.shape[1] * X.shape[2] * X.shape[3]))    
-        return self.clf.predict(X_vectorized)
- 
+        X = self.preprocess(X)
+        return self.net.predict(X)
+
     def predict_proba(self, X):
-        X_vectorized = X.reshape((X.shape[0], X.shape[1] * X.shape[2] * X.shape[3]))    
-        return self.clf.predict_proba(X_vectorized)
+        X = self.preprocess(X)
+        return self.net.predict_proba(X)
+
